b16c610aa959a4504369ceb4ed20afa01e817c06
Ioannis Zannos <zannos@gmail.com>
2015-05-11 00:12:25 +0300

    Published on: Mon May 11 00:12:25 EEST 2015

--- DEPLOY/github.sh
+++ DEPLOY/github.sh
@@ -7,7 +7,7 @@ git checkout master
 git add .
 git commit -am "Published on: `date`"
 git push origin master
-git checkout gh-pages
+git checkout -b gh-pages
 git merge master
 git push origin gh-pages
 git checkout master
