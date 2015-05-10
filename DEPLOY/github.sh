a0190b4337f500317cd295021c29903fa8b316be
Ioannis Zannos <zannos@gmail.com>
2015-05-11 00:10:41 +0300

    testing push

---
+++ DEPLOY/github.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo `date` >> /Users/iani/temp-deploy.txt
+echo `pwd` >> /Users/iani/temp-deploy.txt
+echo $1 >> /Users/iani/temp-deploy.txt
+cd $1
+git checkout master
+git add .
+git commit -am "Published on: `date`"
+git push origin master
+git checkout gh-pages
+git merge master
+git push origin gh-pages
+git checkout master
